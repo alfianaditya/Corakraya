384526b073c91cf46c33fed78fc7c24a7c8349bc
AlfianAditya <alfianaditya730@gmail.com>
2021-09-10 18:45:24 +0700

    haha

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,28 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 } 
 
-// var header = document.querySelector('.header');
-// var oldColor = header.style.backgroundColor;
+var header = document.querySelector('.header');
+var oldColor = header.style.backgroundColor;
 
-// document.body.addEventListener('scroll', () => {
-//     let yPosition = document.body.scrollTop;
-//     if (yPosition > 100) {
-//         header.style.backgroundColor = "#fff";
-//     }
-//     else {
-//         header.style.backgroundColor = oldColor;
-//     }
-// })
-
-const header = document.querySelector('.header');
-
-window.onscroll = function () {
-
-    var top = window.scrollY;
-    console.log(top);
-    if (top >= 100) {
-        header.classList.add('active');
-    } else{
-        header.classList.remove('active');
+document.body.addEventListener('scroll', () => {
+    let yPosition = document.body.scrollTop;
+    if (yPosition > 100) {
+        header.style.backgroundColor = "#fff";
     }
-}
+    else {
+        header.style.backgroundColor = oldColor;
+    }
+})
+
+// const header = document.querySelector('.header');
+
+// window.onscroll = function () {
+
+//     var top = window.scrollY;
+//     console.log(top);
+//     if (top >= 100) {
+//         header.classList.add('active');
+//     } else{
+//         header.classList.remove('active');
+//     }
+// }
